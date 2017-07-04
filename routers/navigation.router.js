4bc5913718cd36360293ba9d389cb88e69027864
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-07-04 15:24:53 +0300

    routers added

---
+++ routers/navigation.router.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+const navigationController = require('../controllers/navigation.controller');
+
+const attach = (app) => {
+    const router = new Router();
+
+    router
+        .get('/', (res, req) => navigationController.getHomePage(res, req))
+        .get('/about', (res, req) => navigationController.getAboutUsPage(res, req))
+        .get('/ranking', (res, req) => navigationController.getRankingListPage(res, req));
+
+    app.use(router);
+};
+
+module.exports = attach;
---
+++ routers/users.router.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+const usersController = require('../controllers/users.controller');
+
+const attach = (app) => {
+    const router = new Router();
+};
+
+module.exports = attach;
