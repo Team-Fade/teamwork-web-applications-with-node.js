1d087f06609335bc1d826c0cc38799631e26d8fd
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-07-04 15:23:51 +0300

    express server configurated

---
+++ config/app.config.js
@@ -0,0 +1,17 @@
+/* globals __dirname */
+
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const configApp = (app) => {
+    app.set('view engine', 'pug');
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use('/libs',
+        express.static(path.join(__dirname, '../node_modules')));
+    app.use('/public',
+        express.static(path.join(__dirname, '../public')));
+};
+
+module.exports = configApp;
