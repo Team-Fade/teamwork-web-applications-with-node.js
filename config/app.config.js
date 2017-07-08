4b3a55d3d0de506642a83f5b50e4ac873c21563d
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-07-08 23:19:52 +0300

    config for the application added

--- config/app.config.js
+++ config/app.config.js
@@ -1,19 +1,29 @@
 /* globals __dirname */
 
-const path = require('path');
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 
-const configApp = (app) => {
+const authentificationConfig = require('./auth.config');
+
+const configureApp = (app) => {
+    authentificationConfig.configureAuthentification(app);
+
     app.set('view engine', 'pug');
+
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({
-        extended: true,
-    }));
-    app.use('/libs',
-        express.static(path.join(__dirname, '../node_modules')));
-    app.use('/public',
-        express.static(path.join(__dirname, '../public')));
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.use('/libs', express.static(path.join(__dirname, '../node_modules/')));
+    app.use('/public', express.static(path.join(__dirname, '../public')));
+
+    app.use(require('connect-flash')());
+    app.use((req, res, next) => {
+        res.locals.messages = require('express-messages')(req, res);
+        next();
+    });
 };
 
-module.exports = configApp;
+module.exports = {
+    configureApp,
+};
