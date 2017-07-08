2394efcce2cc859c001bfbe2d23fb455fd3b71cd
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-07-08 23:27:26 +0300

    gulpfile.js fixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,16 @@
-/* globals process */
-
 const gulp = require('gulp');
 const nodemon = require('gulp-nodemon');
-const constants = require('./utils/constants');
-
-// eslint-disable-next-line no-process-env
-const port = process.env.PORT || constants.port;
 
 gulp.task('server', () => {
-    const app = require('./app');
+    const app = require('./server');
 
-    app.listen(port, () => {
-        console.log(`Server is running on http://localhost:${port}`);
-    });
+    app.start();
 });
 
 gulp.task('dev', ['server'], () => {
     return nodemon({
         ext: 'js',
         tasks: ['server'],
-        script: 'app.js',
+        script: 'server.js',
     });
 });
