61fdff30e46c462bf504acbf3cc245c6055654c1
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-07-08 23:25:34 +0300

    added index.js for exporting in modules from config directory

---
+++ config/index.js
@@ -0,0 +1,11 @@
+const appConfig = require('./app.config');
+const serverConfig = require('./server.config');
+const dbConfig = require('./db.config');
+const authentificationConfig = require('./auth.config');
+
+module.exports = {
+    appConfig,
+    serverConfig,
+    dbConfig,
+    authentificationConfig,
+};
