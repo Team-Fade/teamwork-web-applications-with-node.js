553d9b9609b07c6f31f7f343f99a921a61da6ece
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-07-08 23:22:16 +0300

    models for user and event added

---
+++ models/user.model.js
@@ -0,0 +1,24 @@
+class User {
+    // static isValid(model) {
+    //     return typeof model !== 'undefined' &&
+    //         typeof model.text === 'string' &&
+    //         model.text.length > 3;
+    // }
+
+    static toViewModel(model) {
+        const viewModel = new User();
+        Object.keys(model)
+            .forEach((property) => {
+                console.log(property);
+                viewModel[property] = model[property];
+            });
+
+        return viewModel;
+    }
+
+    get id() {
+        return this._id;
+    }
+}
+
+module.exports = User;
