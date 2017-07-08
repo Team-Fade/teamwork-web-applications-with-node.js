aef4526bff7511c5ac85b13a6e506121b743fb74
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-07-08 23:27:07 +0300

    server.js added

---
+++ server.js
@@ -0,0 +1,20 @@
+const { serverConfig } = require('./config');
+const { dbConfig } = require('./config');
+
+const successMessage = `Server is running on localhost//:${serverConfig.port}`;
+
+const start = () => {
+    return Promise.resolve()
+        .then(() => require('./db').init(dbConfig.connectionString))
+        .then((db) => require('./data').init(db))
+        .then((data) => require('./app').init(data))
+        .then((app) => {
+            app.listen(serverConfig.port,
+                () =>
+                    console.log(successMessage));
+        });
+};
+
+module.exports = {
+    start,
+};
