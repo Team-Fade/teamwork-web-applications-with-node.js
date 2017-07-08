814919ba280d5739120926c145d319f539beaa9b
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-07-08 23:21:32 +0300

    app.js moved in app folder

---
+++ app/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { appConfig } = require('../config');
+
+const init = (data) => {
+    const app = express();
+
+    appConfig.configureApp(app);
+
+    require('./routers')
+        .attachRoutes(app, data);
+
+    return Promise.resolve(app);
+};
+
+module.exports = {
+    init,
+};
