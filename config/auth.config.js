b98f9407834848d8c1dbd016d1cdbad3b014be17
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-07-09 17:03:29 +0300

    Authentication fixed. Now session is added in db. Some cookie options added. SerialiseUser fixed

--- config/auth.config.js
+++ config/auth.config.js
@@ -2,28 +2,62 @@ const passport = require('passport');
 const { Strategy } = require('passport-local');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
-const configureAuthentification = (app) => {
+// Hashing password function
+
+const configureAuthentification = (app, { users }) => {
     passport.use(new Strategy(
         (username, password, done) => {
+            users.collection
+                .findOne({ 'username': username }, (err, user) => {
+                    if (err) {
+                        return done(err);
+                    }
+                    if (!user) {
+                        return done(null, false,
+                            { message: 'Incorrect username.' });
+                    }
 
+                    if (user.password !== password) {
+                        return done(null, false,
+                            { message: 'Incorrect password.' });
+                    }
+
+                    return done(null, user);
+                });
         }
     ));
 
-    app.use(cookieParser());
-    app.use(session({ secret: 'Purple Unicorn' }));
+    // Need to generate session key, add in db and change it on every log
+
     app.use(passport.initialize());
     app.use(passport.session());
+    app.use(cookieParser());
+    app.use(session({
+        secret: 'Purple Unicorn',
+        maxAge: new Date(Date.now() + 60 * 60 * 1000),
+        resave: false,
+        saveUninitialized: false,
+        store: new MongoStore(
+            {
+                url: 'mongodb://localhost/nodejs-teamwork',
+            },
+            (err) => {
+                console.log(err || 'connect-mongodb setup ok');
+            }),
+    }));
 
     passport.serializeUser((user, done) => {
-        done(null, user.id);
+        const id = user._id;
+        done(null, id);
     });
 
     passport.deserializeUser((id, done) => {
-
+        users.collection.findOne({ '_id': id }, (err, user) => {
+            done(err, user);
+        });
     });
 };
 
-module.exports = {
-    configureAuthentification,
-};
+module.exports = configureAuthentification;
