463d2103cd8baad1eabf22be9a2e704f811d895f
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-07-18 14:51:02 +0300

    simple reformatting and renaming

--- config/app.config.js
+++ config/app.config.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const flash = require('connect-flash');
 
 const configureAuthentification = require('./auth.config');
 
@@ -14,9 +15,10 @@ const configureApp = (app, data) => {
     app.use(bodyParser.urlencoded({ extended: true }));
 
     app.use('/libs', express.static(path.join(__dirname, '../node_modules/')));
+    // app.use('/jquery', express.static(__dirname, '/libs/jquery/dist/'));
     app.use('/public', express.static(path.join(__dirname, '../public')));
 
-    app.use(require('connect-flash')());
+    app.use(flash());
     app.use((req, res, next) => {
         res.locals.messages = require('express-messages')(req, res);
         next();
