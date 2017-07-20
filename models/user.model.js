d7a5a52d4cdc10a8115228a66e87f5682d1ffbce
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-07-20 15:03:08 +0300

    validation in user model added.

--- models/user.model.js
+++ models/user.model.js
@@ -1,9 +1,17 @@
 class User {
-    // static isValid(model) {
-    //     return typeof model !== 'undefined' &&
-    //         typeof model.text === 'string' &&
-    //         model.text.length > 3;
-    // }
+    static isValid(model) {
+        console.log(model.password.hasOwnProperty('salt'));
+        return typeof model !== 'undefined' &&
+            typeof model.username === 'string' &&
+            model.username.length > 3 &&
+            model.password.hasOwnProperty('salt') &&
+            model.password.hasOwnProperty('passwordHash') &&
+            typeof model.firstName !== 'undefined' &&
+            typeof model.lastName !== 'undefined' &&
+            typeof model.city !== 'undefined' &&
+            typeof model.email !== 'undefined' &&
+            typeof model.profileImage !== 'undefined';
+    }
 
     static toViewModel(model) {
         const viewModel = new User();
