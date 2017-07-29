3b1e1f7c2253a4b41f171efd9f1c5a97fcbeae88
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-07-29 22:22:09 +0300

    users.controller editProfilePage tests added

--- test/unit/app/controllers/users.controller/editProfilePage.js
+++ test/unit/app/controllers/users.controller/editProfilePage.js
@@ -8,12 +8,12 @@ describe('users controller', () => {
     let req = null;
     let res = null;
 
-    describe('if user is logged in', () => {
+    describe('if user is logged in and email already exists', () => {
         beforeEach(() => {
             data = {
                 users: {
                     getOne() {
-
+                        return Promise.resolve({});
                     },
                     edit() {
 
@@ -23,15 +23,58 @@ describe('users controller', () => {
 
             const reqOptions = {
                 body: {
-                    email: 'newTestEmail',
-
+                    email: 'test@abv.bg',
+                    password: 'newTestPassword',
                 },
                 flash: () => {
 
                 },
-                file: {
-                    path: {
+            };
 
+            controller = init(data);
+
+            req = require('../../../req-res')
+                .getRequestMock(reqOptions);
+            res = require('../../../req-res')
+                .getResponseMock();
+
+            res.locals.user.username = 'testUser';
+        });
+
+        it('expect editProfilePage() to redirect to /user/profile/edit', () => {
+            return controller.editProfilePage(req, res)
+                .then(() => {
+                    expect(res.redirectUrl).to.be.equal('/user/profile/edit');
+                });
+        });
+    });
+
+    describe('if user is logged in and email does not exist', () => {
+        beforeEach(() => {
+            data = {
+                users: {
+                    getOne() {
+                        return Promise.resolve();
+                    },
+                    edit() {
+
+                    },
+                },
+            };
+
+            const reqOptions = {
+                body: {
+                    email: 'test@abv.bg',
+                    password: 'newTestPassword',
+                },
+                flash: () => {
+
+                },
+                session: {
+                    passport: {
+                        user: {
+                            email: '',
+                        },
                     },
                 },
             };
@@ -46,10 +89,11 @@ describe('users controller', () => {
             res.locals.user.username = 'testUser';
         });
 
-        it('expect editProfilePage() to render users/my-events with the events', () => {
-            controller.editProfilePage(req, res)
+        it('expect editProfilePage() to redirect to /user/profile/edit and set in session the new email', () => {
+            return controller.editProfilePage(req, res)
                 .then(() => {
                     expect(res.redirectUrl).to.be.equal('/user/profile/edit');
+                    expect(req.session.passport.user.email).to.be.equal('test@abv.bg');
                 });
         });
     });
