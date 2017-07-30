db9d7b54ae4148d38cd42945dfc068ce55d9db72
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2017-07-30 20:09:37 +0300

    browser tests for home page added

--- test/browser/home/home.header.js
+++ test/browser/home/home.header.js
@@ -3,76 +3,86 @@ const { expect } = require('chai');
 const { setupDriver } = require('../utils/setup-driver');
 const webdriver = require('selenium-webdriver');
 
-describe('Home routes', () => {
+describe('Home route', () => {
     let driver = null;
 
-    const appUrl = 'http://localhost:3002/';
+    const appUrl = 'http://localhost:3002/home';
 
     beforeEach(() => {
         driver = setupDriver('chrome');
+        return driver.get(appUrl);
     });
 
-    it('expect header to have link to home with name "Home"', (done) => {
-        driver.get(appUrl)
-            .then(() => {
-                return driver.findElement(
-                    webdriver.By.id('navbar-home')
-                );
-            })
+    it('expect header to have name element "#navbar"', (done) => {
+        driver.findElement(
+            webdriver.By.css('#navbar'))
+            .then((el) => {
+                expect(el).to.not.be.undefined;
+                done();
+            });
+    });
+
+    it('expect header to have name element "#nav - mobile"', (done) => {
+        driver.findElement(
+            webdriver.By.css('#nav-mobile'))
+            .then((el) => {
+                expect(el).to.not.be.undefined;
+                done();
+            });
+    });
+
+    it('expect header to have name "Fade"', (done) => {
+        driver.findElement(
+            webdriver.By.className('brand-logo'))
             .then((el) => {
                 return el.getText();
             })
             .then((text) => {
-                expect(text).to.contain('Home');
+                expect(text).to.contain('Fade');
                 done();
             });
     });
-    it('expect header to have link to about us page with name "About us"',
+
+    it('expect body to button with text "Login"', (done) => {
+        driver.findElement(
+            webdriver.By.css('#login-btn'))
+            .then((el) => {
+                return el.getText();
+            })
+            .then((text) => {
+                expect(text).to.contain('LOGIN');
+                done();
+            });
+    });
+
+    it(`expect body to conitan text 
+    "Check out our top rated events"`, (done) => {
+            driver.findElement(
+                webdriver.By.css('body > main > div.flow-text > p'))
+                .then((el) => {
+                    return el.getText();
+                })
+                .then((text) => {
+                    expect(text)
+                        .to.deep.contain('Check out our top rated events');
+                    done();
+                });
+        });
+
+    it('expect body to have button with name "Browse more"',
         (done) => {
             driver.get(appUrl)
                 .then(() => {
                     return driver.findElement(
-                        webdriver.By.id('navbar-about')
+                        webdriver.By.css('#browse-events-btn')
                     );
                 })
                 .then((el) => {
                     return el.getText();
                 })
                 .then((text) => {
-                    expect(text).to.contain('About us');
+                    expect(text).to.deep.equal('BROWSE MORE');
                     done();
                 });
-    });
-    it('expect header to have link to about us page with name "Chat"',
-        (done) => {
-            driver.get(appUrl)
-                .then(() => {
-                    return driver.findElement(
-                        webdriver.By.id('navbar-chat')
-                    );
-                })
-                .then((el) => {
-                    return el.getText();
-                })
-                .then((text) => {
-                    expect(text).to.contain('Chat');
-                    done();
-                });
-    });
-    it('expect home to have link to browse events page with name "Browse more"',
-        (done) => {
-            driver.get(appUrl)
-                .then(() => {
-                    return driver.findElement(
-                        webdriver.By.id('browse-events-btn')
-                    );
-                })
-                .then((el) => {
-                    return el.getText();
-                })
-                .then((text) => {
-                    expect(text).to.contain('Browse more');
-                    done();
-                });
-    });
+        });
 });
