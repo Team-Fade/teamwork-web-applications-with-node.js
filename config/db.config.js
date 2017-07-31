04e9e02d8cb27f7639423180c819eb5a00960967
Vlado Vlaev <vlado.vlaev@gmail.com>
2017-07-31 18:56:00 +0300

    Added comments for the live version

--- config/db.config.js
+++ config/db.config.js
@@ -1,4 +1,5 @@
-const connectionString = 'mongodb://172.31.32.192:27017/nodejs-teamwork';
+const connectionString = 'mongodb://localhost:27017/nodejs-teamwork';
+// const connectionString = 'mongodb://172.31.32.192:27017/nodejs-teamwork';
 
 module.exports = {
     connectionString,
